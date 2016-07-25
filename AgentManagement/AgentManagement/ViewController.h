ca7c67703af75567b0c298ab55b2af6260da2c56
linkun <linkun@didichuxing.com>
2016-07-25 16:00:51 +0800

    create project.

---
+++ AgentManagement/AgentManagement/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AgentManagement
+//
+//  Created by Kyle on 16/7/25.
+//  Copyright © 2016年 KK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AgentManagement/AgentManagement/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  AgentManagement
+//
+//  Created by Kyle on 16/7/25.
+//  Copyright © 2016年 KK. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
