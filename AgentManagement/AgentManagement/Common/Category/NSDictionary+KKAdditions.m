1c27dc78c56c26a2989f90538bd60f6219fb6fca
linkun <linkun@didichuxing.com>
2016-08-27 17:20:06 +0800

    add base request, no test

---
+++ AgentManagement/AgentManagement/Common/Category/NSDictionary+KKAdditions.m
@@ -0,0 +1,18 @@
+//
+//  NSDictionary+KKAdditions.m
+//  AgentManagement
+//
+//  Created by Kyle on 16/8/27.
+//  Copyright © 2016年 KK. All rights reserved.
+//
+
+#import "NSDictionary+KKAdditions.h"
+
+@implementation NSDictionary (KKAdditions)
+
+- (NSData *)convertToData
+{
+    return [NSJSONSerialization dataWithJSONObject:self options:NSJSONWritingPrettyPrinted error:nil];
+}
+
+@end
--- AgentManagement/AgentManagement/Request/AMBaseRequest.m
+++ AgentManagement/AgentManagement/Request/AMBaseRequest.m
@@ -7,9 +7,22 @@
 //
 
 #import "AMBaseRequest.h"
-#import <AFNetworking/AFNetworking.h>
+#import "ASIHTTPRequest.h"
+#import "NSDictionary+KKAdditions.h"
+#import "NSData+KKAdditions.h"
 
 static NSString *const kAPIBaseURL = @"http://123.56.10.232:81/index.php?r=";
+
+@interface AMBaseRequest ()<ASIHTTPRequestDelegate>
+
+@property (nonatomic, strong) ASIHTTPRequest *request;
+
+@property (nonatomic, copy) KKRequestSuccess requestSuccess;
+@property (nonatomic, copy) KKRequestFailure requestFailure;
+
+@property (nonatomic, assign) NSUInteger currentRetryCount;
+
+@end
 
 @implementation AMBaseRequest
 
@@ -17,20 +30,29 @@ static NSString *const kAPIBaseURL = @"http://123.56.10.232:81/index.php?r=";
 {
     self = [super init];
     if (self) {
-        self.httpSessionManager.responseSerializer = [AFJSONResponseSerializer serializer];
-        self.httpSessionManager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript", @"text/html", @"text/plain", nil];
+        self.retryCount = 0;
+        self.retryTimeInterval = 0.;
+        self.isUseCache = NO;
     }
     return self;
-}
-
-- (KKHttpMethodType)httpMethodType
-{
-    return KKHttpMethodType_POST;
 }
 
 - (NSDictionary *)commonParameters
 {
     return @{};
+}
+
+- (NSMutableDictionary *)requestParameters
+{
+    if (!_requestParameters) {
+        _requestParameters = [NSMutableDictionary dictionary];
+    }
+    return _requestParameters;
+}
+
+- (KKHttpMethodType)httpMethodType
+{
+    return KKHttpMethodType_POST;
 }
 
 - (NSString *)apiBaseURL
@@ -40,7 +62,181 @@ static NSString *const kAPIBaseURL = @"http://123.56.10.232:81/index.php?r=";
 
 - (NSString *)urlPath
 {
-    return @"";
+    return nil;
+}
+
+- (id)buildModelWithJsonDictionary:(NSDictionary *)dictionary
+{
+    return nil;
+}
+
+- (void)requestWithSuccess:(KKRequestSuccess)success failure:(KKRequestFailure)failure
+{
+    self.requestSuccess = success;
+    self.requestFailure = failure;
+    switch ([self httpMethodType]) {
+        case KKHttpMethodType_GET: {
+            [self makeGetRequest];
+            
+            break;
+        } case KKHttpMethodType_POST: {
+            [self makePostRequest];
+            
+            break;
+        } default:
+            break;
+    }
+}
+
+- (NSDictionary *)buildParameters
+{
+    NSMutableDictionary *httpDictionary = [[self commonParameters] mutableCopy];
+    
+    for (id key in self.requestParameters) {
+        [httpDictionary setObject:self.requestParameters[key] forKey:key];
+    }
+    return [httpDictionary copy];
+}
+
+- (NSString *)buildRequestURL
+{
+    NSAssert(([self apiBaseURL] != nil), @"apiBaseURL不能为空");
+    NSAssert(([self urlPath] != nil), @"urlPath不能为空");
+    NSString *requestURL = [NSString stringWithFormat:@"%@%@", [self apiBaseURL], [self urlPath]];
+    
+    return requestURL;
+}
+
+- (void)makeGetRequest
+{
+    NSDictionary *parameters = [self buildParameters];
+    
+    self.request = [ASIHTTPRequest requestWithURL:[NSURL URLWithString:[self buildRequestURL]]];
+    self.request.requestMethod = @"GET";
+    self.request.allowCompressedResponse = YES;
+    self.request.delegate = self;
+    if ([parameters allKeys].count > 0) {
+        for (NSString *key in [parameters allKeys]) {
+            [self.request addRequestHeader:key value:parameters[key]];
+        }
+    }
+    [self.request startAsynchronous];
+}
+
+- (void)makePostRequest
+{
+    NSDictionary *parameters = [self buildParameters];
+    
+    self.request = [ASIHTTPRequest requestWithURL:[NSURL URLWithString:[self buildRequestURL]]];
+    self.request.requestMethod = @"POST";
+    self.request.allowCompressedResponse = YES;
+    self.request.delegate = self;
+    [self.request setPostBody:[NSMutableData dataWithData:[parameters convertToData]]];
+    [self.request startAsynchronous];
+}
+
+- (void)handleResultWithRequest:(ASIHTTPRequest *)request
+{
+    if (request.responseStatusCode == 200) {
+        [self handleSuccessWithRequest:request];
+    } else {
+        [self handleFailureWithRequest:request];
+    }
+}
+
+- (void)handleSuccessWithRequest:(ASIHTTPRequest *)request
+{
+    NSDictionary *responseDictionary = [request.responseData convertToDictionary];
+    
+    if (responseDictionary) {
+        if (self.isUseCache) {
+            [self cacheResponseObject:responseDictionary];
+        }
+        
+        if (self.requestSuccess) {
+            self.requestSuccess([self buildModelWithJsonDictionary:responseDictionary], nil);
+            self.requestSuccess = nil;
+        }
+    } else {
+        DDLogError(@"responseDictionary为空");
+    }
+}
+
+- (void)handleFailureWithRequest:(ASIHTTPRequest *)request
+{
+    if ((self.retryCount > 0) && (++self.currentRetryCount < self.retryCount)) {
+#warning dispatch_after 若interval为0，调用为同步还是异步?
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(self.retryTimeInterval * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [self requestWithSuccess:self.requestSuccess failure:self.requestFailure];
+        });
+        return;
+    }
+    
+    if (self.requestFailure) {
+        self.requestFailure(nil, [[KKRequestError alloc] initWithError:request.error]);
+        self.requestFailure = nil;
+    }
+}
+
+- (void)cancel
+{
+    self.requestSuccess = nil;
+    self.requestFailure = nil;
+    self.request.delegate = nil;
+    [self.request cancel];
+}
+
+#pragma mark - KKCache
+
+- (NSString *)cacheFileDirectory
+{
+    return nil;
+}
+
+- (NSString *)cacheFilePath
+{
+    return nil;
+}
+
+- (id)getCacheObject
+{
+    NSString *directory = [self cacheFileDirectory];
+    NSString *path = [self cacheFilePath];
+    
+    NSAssert(((directory != nil) && (path != nil)), @"缓存文件夹&文件路径不能为空, cacheFileDirectory = %@, cacheFilePath = %@", directory, path);
+    NSDictionary *dictionary = [NSKeyedUnarchiver unarchiveObjectWithFile:[NSString stringWithFormat:@"%@%@", directory, path]];
+    
+    if (dictionary) {
+        return [self buildModelWithJsonDictionary:dictionary];
+    } else {
+        return nil;
+    }
+}
+
+- (void)cacheResponseObject:(NSDictionary *)responseObject
+{
+#warning 进一步明确archive的使用及存储数据形式
+    NSString *directory = [self cacheFileDirectory];
+    NSString *path = [self cacheFilePath];
+    
+    NSAssert(((directory != nil) && (path != nil)), @"缓存文件夹&文件路径不能为空, cacheFileDirectory = %@, cacheFilePath = %@", directory, path);
+    [NSKeyedArchiver archiveRootObject:responseObject toFile:[NSString stringWithFormat:@"%@%@", directory, path]];
+}
+
+#pragma mark - ASIHTTPRequestDelegate
+
+- (void)requestFinished:(ASIHTTPRequest *)request
+{
+    [self handleResultWithRequest:request];
+}
+
+//- (void)request:(ASIHTTPRequest *)request didReceiveData:(NSData *)data
+//{
+//}
+
+- (void)requestFailed:(ASIHTTPRequest *)request
+{
+    [self handleResultWithRequest:request];
 }
 
 @end
