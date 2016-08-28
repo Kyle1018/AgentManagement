42c9ef2901a68d393d299a2a178e1f1cbbde8b13
guohuabin0215 <979168709@qq.com>
2016-08-28 16:38:19 +0800

    侧滑菜单视图

--- AgentManagement/AgentManagement/Common/CommonView/MenuView.h
+++ AgentManagement/AgentManagement/Common/CommonView/MenuView.h
@@ -8,7 +8,44 @@
 
 #import <UIKit/UIKit.h>
 
+
+
 @interface MenuView : UIView
+
+@property (weak, nonatomic) IBOutlet UITableView *menuTabelView;
+
+
++ (instancetype)createFromXibWithViewTag:(NSInteger)tag ToAddView:(UIView*)view;
+
+- (void)show;
+
+//- (void)hide;
+
+
+
+@end
+
+@interface MenuViewCell : UITableViewCell
+
+
++ (id)createFromXibWithMenuViewCell;
+
+@property (weak, nonatomic) IBOutlet UIButton *optionA;
+
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *optionB;
+
+
+@end;
+
+
+
+
+typedef void(^TapAllButtonBlock)();
+@interface MenuCellHeaderView : UIView
+
++ (id)createFromXibWithMenuCellHeaderView;
+
+@property(nonatomic,copy)TapAllButtonBlock tapAllButtonBlock;
 
 
 @end
