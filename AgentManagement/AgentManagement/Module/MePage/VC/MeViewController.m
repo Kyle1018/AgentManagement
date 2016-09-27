6054b9f7f7c4afc471aa1a93b682428fdef21544
linkun <linkun@didichuxing.com>
2016-09-27 09:55:42 +0800

    去除无关代码

--- AgentManagement/AgentManagement/Module/MePage/VC/MeViewController.m
+++ AgentManagement/AgentManagement/Module/MePage/VC/MeViewController.m
@@ -7,7 +7,33 @@
 //
 
 #import "MeViewController.h"
+#import <SDWebImage/UIImageView+WebCache.h>
+
+@interface MeViewController ()
+
+@property (nonatomic, weak) IBOutlet UIImageView *portraitImageView;
+@property (nonatomic, weak) IBOutlet UILabel *nameLabel;
+@property (nonatomic, weak) IBOutlet UILabel *phoneLabel;
+@property (nonatomic, weak) IBOutlet UILabel *addressLabel;
+@property (nonatomic, weak) IBOutlet UIImageView *codeImageView;
+
+@end
 
 @implementation MeViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self initializeControl];
+}
+
+- (void)initializeControl
+{
+    [self.portraitImageView sd_setImageWithURL:[NSURL URLWithString:@""] placeholderImage:[UIImage imageNamed:@""]];
+    self.nameLabel.text = nil;
+    self.phoneLabel.text = nil;
+    self.addressLabel.text = nil;
+    [self.codeImageView sd_setImageWithURL:[NSURL URLWithString:@""] placeholderImage:[UIImage imageNamed:@""]];
+}
 
 @end
