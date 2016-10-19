956dd24eeb5895f3d07317ee4c81ab25b58cc5f1
linkun <linkun@didichuxing.com>
2016-10-19 18:55:49 +0800

    添加用户设置页面

---
+++ AgentManagement/AgentManagement/Module/MePage/Setting/Certification/AMCertificationViewController.m
@@ -0,0 +1,27 @@
+//
+//  AMCertificationViewController.m
+//  AgentManagement
+//
+//  Created by Kyle on 2016/10/19.
+//  Copyright © 2016年 KK. All rights reserved.
+//
+
+#import "AMCertificationViewController.h"
+
+@interface AMCertificationViewController ()
+
+@end
+
+@implementation AMCertificationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
